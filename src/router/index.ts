d06c5da38261713a361a36e82220bbce44f06fdb
VAV <avabvnn@gmail.com>
2025-02-04 16:26:57 +0300

    shims-vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PostList from '@/components/PostList.vue';
 import PostDetail from '@/components/PostDetail.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
